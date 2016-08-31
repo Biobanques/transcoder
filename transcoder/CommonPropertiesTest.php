5677fc54d3530f51233b77ba9dda74a44861c5f7
mpenicaud <matthieu.penicaud@inserm.fr>
2016-08-31 13:39:07 +0200

    update config to add database to circleCi

---
+++ transcoder/CommonPropertiesTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+  Common properties for circle ci
+ */
+
+class CommonProperties
+{
+    static $CONNECTIONSTRING = "mysql:host=localhost;dbname=circle_test";
+    static $CONNECTIONUSER = "ubuntu";
+    static $CONNECTIONPASSWORD = "";
+}
--- transcoder/protected/tests/bootstrap.php
+++ transcoder/protected/tests/bootstrap.php
@@ -4,9 +4,11 @@
 $yiit = dirname(__FILE__) . '/../../yii-1.1.14/framework/yiit.php';
 $config = dirname(__FILE__) . '/../config/test.php';
 $htmlPurifier = dirname(__FILE__) . '/../../yii-1.1.14/framework/vendors/htmlpurifier/HTMLPurifier.standalone.php';
+$commonProperties = dirname(__FILE__) . '/../../CommonProperties.php';
 
 require_once($yiit);
 
+include $commonProperties;
 
 require_once ($htmlPurifier);
 Yii::createWebApplication($config);
